375783a5ebe86dc73728bc0afa9e99c1e4ac0ede
PromiSe#### <vicimpa@ya.ru>
2023-02-15 20:26:42 +0700

    Fix footer message

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,6 @@ createRoot(
   <>
     <img src={logoIcon} />
     <p>Site is comming soon...</p>
-    <span>© RuStudentDev 2023 <a title="My telegram" href="https://t.me/vic_dev">@vic_dev</a></span>
+    <span><a href="https://github.com/rustudentdev">RuStudentDev</a> © 2023 <a title="My telegram" href="https://t.me/vic_dev">@vic_dev</a></span>
   </>
 );
