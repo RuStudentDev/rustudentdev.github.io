2b2ff022ffaefa3ea7f2c0ce40927e7ea04bfbce
PromiSe#### <vicimpa@ya.ru>
2023-02-15 19:48:26 +0700

    Base app

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,12 @@
-console.log("Hello world!");
-export default undefined;
+import logoIcon from "images/logo.svg";
+import { getElementById } from "lib/getElementById";
+import { createRoot } from "react-dom/client";
+
+createRoot(
+  getElementById('root')!
+).render(
+  <>
+    <img src={logoIcon} />
+    <p>Site is comming soon...</p>
+  </>
+);
